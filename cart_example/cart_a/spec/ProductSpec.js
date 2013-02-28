d606b1c3d7f79b58cf6a1e954356f12170973219
Marco Emrich <marcoemrich77@googlemail.com>
2013-02-28 16:25:01 +0100

    fixed a missing var

--- cart_example/cart_a/spec/ProductSpec.js
+++ cart_example/cart_a/spec/ProductSpec.js
@@ -1,7 +1,7 @@
 describe('A Product', function() {
 
     it('should calculate its #grossPrice by adding a VAT of 20%', function() {
-        product = Product.create('Some Product', 100)
+        var product = Product.create('Some Product', 100)
         expect(product.grossPrice()).toEqual(120);
     });
 
